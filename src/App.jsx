1d3eec88c70f7154ea146b497b19e4344dab1ba4
Anand Mainali <anand.mainali@cyberarrow.io>
2023-11-19 11:30:00 +0545

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import React from 'react'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import About from './pages/About'
 
-
-function App() {
-  
-
+export default function App() {
   return (
-    <>
-      <h2 className='bg-red-300'>hello</h2>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/sign-in' element={<Signin/>}/>
+        <Route path='/sign-up' element={<Signup/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
   )
-}
 
-export default App
+}
