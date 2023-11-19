f5fb2cf963b6f3f459daced52bbcb60b2ee58263
Anand Mainali <anand.mainali@cyberarrow.io>
2023-11-19 12:39:45 +0545

    creating and running the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import Profile from './pages/Profile'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import About from './pages/About'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/profile' element={<Profile/>}/>
@@ -18,5 +19,4 @@ export default function App() {
       </Routes>
     </BrowserRouter>
   )
-
 }
