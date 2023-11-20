6cb676e406a225283f5cd312178461e45f047ae4
Anand Mainali <anand.mainali@cyberarrow.io>
2023-11-20 13:19:13 +0545

    creating a modal

--- api/models/user.modal.js
+++ api/models/user.modal.js
@@ -15,6 +15,6 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true
     }
-})
+},{timestamps:true})
 
 export default userModel = mongoose.model('User',userSchema)
