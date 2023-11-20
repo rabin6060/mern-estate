0c878fec264da78d26ecf1b95460e3de6c6feb30
Anand Mainali <anand.mainali@cyberarrow.io>
2023-11-20 13:16:52 +0545

    creating a user modal.

---
+++ api/models/user.modal.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+export default userModel = mongoose.model('User',userSchema)
