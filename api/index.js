93897b151ef3c5ec59a31ce0769cbe4f54638b14
Anand Mainali <anand.mainali@cyberarrow.io>
2023-11-20 13:09:06 +0545

    create a database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,22 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
 
-const app = express()
+const connect = async() => {
+  await mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+}
 
 
+const app = express()  
 
 app.listen(3000,()=>{
-    console.log("server running on the port 3000")
+    console.log(`server running on the port ${process.env.PORT}`)
+    connect()
 })
