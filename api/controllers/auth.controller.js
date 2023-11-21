00ca9e2843d08bfa1c891bbf9743943d3430e223
Anand Mainali <anand.mainali@cyberarrow.io>
2023-11-21 12:47:07 +0545

    create a signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.modal.js"
 import bcrypt from 'bcryptjs'
 import { errorHandler } from "../utils/error.js"
+import jwt from 'jsonwebtoken'
 
 export const signup =async (req,res,next) => {
     const {username,email,password} = req.body
@@ -13,5 +14,23 @@ export const signup =async (req,res,next) => {
     } catch (error) {
         next(error)
     }
-
+}
+export const signin  = async(req,res,next) => {
+    const {email,password} = req.body
+    try {
+        const user = await User.findOne({email})
+        if (!user) return next(errorHandler(404,"user not found"))
+        const passwordCheck = bcrypt.compareSync(password,user.password)
+        if (!passwordCheck) return next(errorHandler(401,"wrong credientials."))
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
+        const {password:pass,...rest}=user._doc
+        res
+        .cookie('access_token',token,{httpOnly:true})
+        .status(200)
+        .json(rest)
+        res.status(200).json("login successfully.")
+        
+    } catch (error) {
+        next(error)
+    }
 }
