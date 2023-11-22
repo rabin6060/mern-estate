ff9077aad54152f99077e64371b4c2d144c44438
Anand Mainali <anand.mainali@cyberarrow.io>
2023-11-22 10:47:58 +0545

    complete profile ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import {useSelector} from 'react-redux'
 
 const Profile = () => {
+  const {currentUser} = useSelector(state=>state.user)
   return (
-    <div>Profile</div>
+    <div className='p-2 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <img className='rounded-full w-24 h-24 object-cover cursor-pointer self-center mt-2 ' src={currentUser.avatar} alt="profile" />
+        <input className='p-3 border rounded-lg ' type="text" placeholder='username' id='username' />
+        <input className='p-3 border rounded-lg ' type="email" placeholder='email' id='email' />
+        <input className='p-3 border rounded-lg ' type="text" placeholder='password' id='password' />
+        <button className='buttons'>update</button>
+      </form>
+      <div className='flex justify-between mt-3'>
+        <span className='text-red-700 cursor-pointer'>Delete Account</span>
+        <span className='text-red-700 cursor-pointer'>Sign Out</span>
+      </div>
+    </div>
   )
 }
 
