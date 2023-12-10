50538015f06f5a5b22a55e77a91825f9928ce57c
Anand Mainali <anand.mainali@cyberarrow.io>
2023-12-10 12:00:25 +0545

    some minor changes

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,9 +1,9 @@
-import { listingModel } from "../models/listing.model.js"
+import { Listing } from "../models/listing.model.js"
 //import { errorHandler } from "../utils/error.js"
 
 export const createListing =async (req,res,next) => {
     try {
-        const listing = await listingModel.create(req.body)
+        const listing = await Listing.create(req.body)
         res.status(201).json(listing)
     } catch (error) {
         next(error)
