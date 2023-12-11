17a5e64675340b463c0f1586d765107fc3e6f267
Anand Mainali <anand.mainali@cyberarrow.io>
2023-12-11 12:10:45 +0545

    completed search api route.

--- api/routes/listing.routes.js
+++ api/routes/listing.routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createListing, DeleteListing, getListing, UpdateListing } from '../controllers/listing.controller.js'
+import { createListing, DeleteListing, getListing, getListings, UpdateListing } from '../controllers/listing.controller.js'
 import {verifyToken} from '../utils/verifyToken.js'
 
 const router = express.Router()
@@ -8,6 +8,7 @@ router.post('/create',verifyToken,createListing)
 router.delete('/delete/:id',verifyToken,DeleteListing)
 router.post('/update/:id',verifyToken,UpdateListing)
 router.get('/get/:id',getListing)
+router.get('/get',getListings)
 
 export default router
 
